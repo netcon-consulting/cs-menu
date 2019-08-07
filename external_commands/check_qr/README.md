4d464dc4e22b1b11da7dab1931d8e6f75b07d3ca
Marc Dierksen <m.dierksen@netcon-consulting.com>
2019-08-07 12:43:48 +0200

    Updated 'check_qr' external command.

--- external_commands/check_qr/README.md
+++ external_commands/check_qr/README.md
@@ -1,4 +1,6 @@
-check_qr.sh V1.3.0
+check_qr.sh V1.4.0
 ==================
 
 Checks images for included QR-codes and prints encoded URLs.
+
+Trusted URLs can be whitelisted in the file '/opt/cs-gateway/scripts/netcon/whitelist_qr.txt'.
--- external_commands/check_qr/check_qr.sh
+++ external_commands/check_qr/check_qr.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-# check_qr.sh V1.3.0
+# check_qr.sh V1.4.0
 #
 # Copyright (c) 2019 NetCon Unternehmensberatung GmbH, netcon-consulting.com
 #
 # Author: Marc Dierksen (m.dierksen@netcon-consulting.com)
 
 # return codes:
-# 0 - picture does not contain QR code with URL link
-# 1 - picture contains QR code with URL link
+# 0 - picture does not contain QR code with non-whitelisted URL link
+# 1 - picture contains QR code with non-whitelisted URL link
 # 99 - unrecoverable error
 
 LOG_PREFIX='>>>>'
 LOG_SUFFIX='<<<<'
+
+FILE_WHITELIST='/opt/cs-gateway/scripts/netcon/whitelist_qr.txt'
 
 # writes message to log file with the defined log pre-/suffix 
 # parameters:
@@ -42,11 +44,13 @@ fi
 
 RESULT="$(zbarimg "$1" 2>/dev/null)"
 
-if [ "$?" = 0 ] && echo "$RESULT" | grep -q 'QR-Code:'; then
-    URL="$(echo "$RESULT" | grep 'QR-Code:' | awk -F 'QR-Code:' '{print $2}' | grep -E '(http|www)')"
+if [ "$?" = 0 ] && echo "$RESULT" | grep -q '^QR-Code:'; then
+    URL="$(echo $RESULT | awk 'match($0, /((https?:\/\/|www\.)[^ ]+)/, a) {print a[1]}')"
     if ! [ -z "$URL" ]; then
-        write_log "$URL"
-        exit 1
+        if ! [ -f "$FILE_WHITELIST" ] || ! grep -q "^$URL$" "$FILE_WHITELIST"; then
+            write_log "$URL"
+            exit 1
+        fi
     fi
 fi
 
